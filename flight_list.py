13c9e9496d3a266c1491f2ae95a1151929484a25
FernandoGomesK <fernandogomest94@gmail.com>
2025-07-06 23:44:32 -0300

    now shows a pilot and copilot

--- flight_list.py
+++ flight_list.py
@@ -10,17 +10,14 @@ class flight_list:
         
     def add_flight(self, flight):
         self.flights.append(flight)  
-    def initialize(self):
         
-        for i in range(5):
-            fly = Flight(2)
+    def initialize(self, flights = 5, passenger_number = 2 ):
+        for i in range(flights):
+            fly = Flight(passenger_number)
             self.add_flight(fly)
-            passengers = [Passenger(fake.name()) for x in range(2)]
-            
-            
+            passengers = [Passenger(fake.name()) for x in range(passenger_number)]
             for p in passengers:
-                fly.assign_passenger(p)
-                
+                fly.assign_passenger(p)   
             fly.display_manifest()
         
  
